2acb86f61ecc63bd9b997414817e1eec0e6371f8
Vivek Acharya <acharyavivek51@gmail.com>
2026-03-29 08:58:56 +0530

    Upgraded organizer: input + safe copy + more file types

--- organizer.py
+++ organizer.py
@@ -1,25 +1,25 @@
 import os
 import shutil
 
-source_folder = "test_folder"
+source_folder = input("Enter folder path: ")
 
 images = os.path.join(source_folder, "Images")
 docs = os.path.join(source_folder, "Documents")
+videos = os.path.join(source_folder, "Videos")
 
 os.makedirs(images, exist_ok=True)
 os.makedirs(docs, exist_ok=True)
+os.makedirs(videos, exist_ok=True)
 
-# create test files
-open(os.path.join(source_folder, "photo.jpg"), "w").close()
-open(os.path.join(source_folder, "file.pdf"), "w").close()
-
-# move files
 for file in os.listdir(source_folder):
 	file_path = os.path.join(source_folder, file)
 
-	if file.endswith(".jpg"):
-		shutil.move(file_path, os.path.join(images, file))
-	elif file.endswith(".pdf"):
-		shutil.move(file_path, os.path.join(docs, file))
+	if os.path.isfile(file_path):
+		if file.endswith((".jpg", "/png")):
+			shutil.copy(file_path, os.path.join(images, file))
+		elif file.endswith((".pdf", ".txt")):
+			shutil.copy(file_path, os.path.join(docs, file))
+		elif file.endswith((".mp4", ".mkv")):
+			shutil.copy(file_path, os.path.join(videos, file))
 
-print("Files organized successfully!")
+print("Files organized safely (copied)!")
