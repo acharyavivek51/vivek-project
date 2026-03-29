4693bc6188a2032663e5788f04ee188ea01cc739
Vivek Acharya <acharyavivek51@gmail.com>
2026-03-29 09:11:33 +0530

    Improved organized with cleaner structure

--- organizer.py
+++ organizer.py
@@ -3,23 +3,26 @@ import shutil
 
 source_folder = input("Enter folder path: ")
 
-images = os.path.join(source_folder, "Images")
-docs = os.path.join(source_folder, "Documents")
-videos = os.path.join(source_folder, "Videos")
+file_types = {
+	"Images": [".jpg", ".png"],
+	"Documents": [".pdf", ".txt"],
+	"Videos": [".mp4", ".mkv"]
+}
 
-os.makedirs(images, exist_ok=True)
-os.makedirs(docs, exist_ok=True)
-os.makedirs(videos, exist_ok=True)
+for folder_name, extensions in file_types.items():
+	folder_path = os.path.join(source_folder, folder_name)
+	os.makedirs(folder_path, exist_ok=True)
 
 for file in os.listdir(source_folder):
 	file_path = os.path.join(source_folder, file)
 
 	if os.path.isfile(file_path):
-		if file.endswith((".jpg", "/png")):
-			shutil.copy(file_path, os.path.join(images, file))
-		elif file.endswith((".pdf", ".txt")):
-			shutil.copy(file_path, os.path.join(docs, file))
-		elif file.endswith((".mp4", ".mkv")):
-			shutil.copy(file_path, os.path.join(videos, file))
+		for folder_name, extensions in file_types.items():
+			if file.endswith(extensions):
+				shutil.copy(file_path, os.path.join(source_folder, folder_name, file))
 
-print("Files organized safely (copied)!")
+print("Files organized successfully!")
+
+
+
+
